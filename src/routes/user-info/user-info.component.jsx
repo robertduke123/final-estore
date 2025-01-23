dbe820370dff0ed3eabe5af2a520a37186a0e4f3
Robert Duke <robduke123@gmail.com>
2025-01-24 00:08:07 +1300

    user info started

--- src/routes/user-info/user-info.component.jsx
+++ src/routes/user-info/user-info.component.jsx
@@ -1,50 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./user-info.styles.scss";
-import FormInput from "../../components/form-input/form-input.component";
-import Button from "../../components/button/button.component";
+import UserForm from "../../components/user-form/user-form.component";
+import UserDisplay from "../../components/user-display/user-display.component";
 
 const UserInfo = () => {
+	const [edit, setEdit] = useState(true);
+
 	return (
 		<div className="user-info-container">
 			<h2>User Info</h2>
-			<form>
-				<FormInput
-					label="Display Name"
-					type="text"
-					required
-					// onChange={}
-					name="displayName"
-					// value={displayName}
-				/>
-
-				<FormInput
-					label="Email"
-					type="text"
-					required
-					// onChange={}
-					name="email"
-					// value={email}
-				/>
-
-				<FormInput
-					label="Password"
-					type="password"
-					required
-					// onChange={}
-					name="password"
-					// value={password}
-				/>
-
-				<FormInput
-					label="Confirm Password"
-					type="password"
-					required
-					// onChange={}
-					name="confirmPassword"
-					// value={confirmPassword}
-				/>
-				<Button type="submit">Sign Up</Button>
-			</form>
+			{edit ? <UserDisplay /> : <UserForm />}
 		</div>
 	);
 };
