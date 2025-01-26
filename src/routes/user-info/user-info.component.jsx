d6cdfa1b276e0b69708327affc9e86c59ce92c7e
Robert Duke <robduke123@gmail.com>
2025-01-26 23:44:49 +1300

    user info started

--- src/routes/user-info/user-info.component.jsx
+++ src/routes/user-info/user-info.component.jsx
@@ -5,11 +5,12 @@ import UserDisplay from "../../components/user-display/user-display.component";
 
 const UserInfo = () => {
 	const [edit, setEdit] = useState(true);
+	const handleEdit = () => setEdit(!edit);
 
 	return (
 		<div className="user-info-container">
 			<h2>User Info</h2>
-			{edit ? <UserDisplay /> : <UserForm />}
+			{edit ? <UserDisplay handleEdit={handleEdit} /> : <UserForm />}
 		</div>
 	);
 };
