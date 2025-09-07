ac41ecd8cb18eba243d6eb5adc8c89d8f935193a
Robert Duke <robduke123@gmail.com>
2025-09-07 20:12:32 +1200

    finished order confirmation component

--- src/routes/orders/orders.components.jsx
+++ src/routes/orders/orders.components.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./orders.styles.scss";
 
 const Orders = () => {
-	return <div></div>;
+	return (
+		<div className="orders-container">
+			<h1>Orders</h1>
+			<ul className="order-list-container"></ul>
+		</div>
+	);
 };
 
 export default Orders;
